5a3112cc5c035a6f768283e7ae64d2b619786ef4
2289580381@qq.com <2289580381@qq.com>
2023-09-30 09:57:38 +0800

    主题色配置

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  
-
-  
+  <el-button type="primary">Primary</el-button>
 </template>
 
 <style scoped>
