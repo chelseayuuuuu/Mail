90b8fdd803ea052fde617c3335ea3141b94820bd
2289580381@qq.com <2289580381@qq.com>
2023-09-30 16:03:41 +0800

    home分层，分类渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,12 +1,17 @@
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
+</script>
+
 <template>
-    <div class="id">
-        home
-    </div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
 </template>
-
-<style scoped>
-.id{
-    height: 1000px;
-}
-
-</style>
