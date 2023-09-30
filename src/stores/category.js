c40de4a9dc3a02a326d444916330ef83f576ac03
2289580381@qq.com <2289580381@qq.com>
2023-09-30 15:43:46 +0800

    pinia管理数据

---
+++ src/stores/category.js
@@ -0,0 +1,15 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { getCategoryAPI } from '@/apis/getCategoryAPI';
+
+export const useCategoryStore = defineStore('category', () => {
+    const categoryList = ref([])
+    const getCategory = async() =>{
+      const res = await getCategoryAPI()
+      categoryList.value = res.result
+    }
+    return {
+        categoryList,
+        getCategory
+    }
+})
--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,18 +1,7 @@
 <script setup>
+import { useCategoryStore } from '@/stores/category';
 
-import { getCategoryAPI } from '@/apis/getCategoryAPI';
-import { onMounted,ref } from 'vue';
-
-const categoryList = ref([])
-const getCategory = async() =>{
-  const res = await getCategoryAPI()
-  categoryList.value = res.result
-}
-
-onMounted(() =>{
-  getCategory()
-})
-
+const categoryStore = useCategoryStore()
 </script>
 
 <template>
@@ -22,7 +11,7 @@ onMounted(() =>{
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home" v-for="item in categoryList" :key="item.id">
+        <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
           <RouterLink to="/">{{ item.name }}</RouterLink>
         </li>
         
