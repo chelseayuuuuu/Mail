bd3128a45acec97e85257210271efae077b9ccbb
2289580381@qq.com <2289580381@qq.com>
2023-09-30 20:48:30 +0800

    一级分类路由配置，面包屑导航渲染，轮播图实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,3 +1,139 @@
+<script setup>
+import { getCategoryAPI } from "@/apis/categorys";
+import { useRoute } from "vue-router";
+import { onMounted, ref } from "vue";
+import { getBannerAPI } from "@/apis/home";
+
+const categoryDate = ref({});
+const route = useRoute();
+const getCategory = async () => {
+  const res = await getCategoryAPI(route.params.id);
+  categoryDate.value = res.result;
+};
+
+const bannerList = ref([])
+const getBanner = async() =>{
+   const res = await getBannerAPI({
+    distributionSite: '2'
+   })
+   bannerList.value = res.result
+}
+
+onMounted(() => {
+  getCategory()
+  getBanner()
+});
+</script>
+
 <template>
-    category
+  <div class="top-category">
+    <div class="container m-top-20">
+      <!-- 面包屑 -->
+      <div class="bread-container">
+        <el-breadcrumb separator=">">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ categoryDate.name }}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="" />
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+    </div>
+  </div>
 </template>
+
+
+<style scoped lang="scss">
+.top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+
+      li {
+        width: 168px;
+        height: 160px;
+
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+
+          img {
+            width: 100px;
+            height: 100px;
+          }
+
+          p {
+            line-height: 40px;
+          }
+
+          &:hover {
+            color: $xtxColor;
+          }
+        }
+      }
+    }
+  }
+
+  .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+
+    .head {
+      .xtx-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+
+      .tag {
+        text-align: center;
+        color: #999;
+        font-size: 20px;
+        position: relative;
+        top: -20px;
+      }
+    }
+
+    .body {
+      display: flex;
+      justify-content: space-around;
+      padding: 0 40px 30px;
+    }
+  }
+
+  .bread-container {
+    padding: 25px 0;
+  }
+  .home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+}
+</style>
