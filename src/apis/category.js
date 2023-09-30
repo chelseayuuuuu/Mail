ea2296fb157feae6eede1bee76891de4863791e8
2289580381@qq.com <2289580381@qq.com>
2023-09-30 22:24:15 +0800

    二级分类-面包屑导航

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,10 +1,19 @@
 import httpInstance from "@/utils/http";
 
 export function getCategoryAPI(id) {
-    return httpInstance({
-        url: '/category',
-        params: {
-            id
-        }
-    })
+  return httpInstance({
+    url: "/category",
+    params: {
+      id,
+    },
+  });
 }
+
+export const getCategoryFilterAPI = (id) => {
+  return httpInstance({
+    url: "/category/sub/filter",
+    params: {
+      id,
+    },
+  });
+};
