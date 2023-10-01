578e6636f9467e459af7afd5dd6da92354ac6327
2289580381@qq.com <2289580381@qq.com>
2023-10-01 11:05:56 +0800

    列表筛选，列表无限加载

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,43 +1,76 @@
 <script setup>
-import { getCategoryFilterAPI } from '@/apis/category';
-import { onMounted,ref } from 'vue';
-import { useRoute } from 'vue-router'
+import { getCategoryFilterAPI, getSubCategoryAPI } from "@/apis/category";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import GoodsItem from "@/views/Home/components/GoodsItem.vue";
 
-const route = useRoute()
-const categoryData = ref({})
-const getCategoryData = async () =>{
-    const res = await getCategoryFilterAPI(route.params.id)
-    categoryData.value = res.result
+const route = useRoute();
+const categoryData = ref({});
+const getCategoryData = async () => {
+  const res = await getCategoryFilterAPI(route.params.id);
+  categoryData.value = res.result;
+};
+
+onMounted(() => {
+  getCategoryData();
+});
+
+const goodList = ref([]);
+const reqData = ref({
+  categoryId: route.params.id,
+  page: 1,
+  pageSize: 20,
+  sortField: "publishTime",
+});
+const getGoodList = async () => {
+  const res = await getSubCategoryAPI(reqData.value);
+  goodList.value = res.result.items;
+};
+
+onMounted(() => getGoodList());
+
+const tabChange = () => {
+  reqData.value.page = 1;
+  getGoodList();
+};
+
+const disabled = ref(false)
+const load = async () =>{
+  // console.log('加载更多');
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value);
+  goodList.value = [...goodList.value, ...res.result.items]
+  // 加载完毕 停止监听
+  if(res.result.items.length === 0){
+    disabled.value = true
+  }
 }
-
-onMounted(() =>{
-    getCategoryData()
-})
 </script>
 
 <template>
-  <div class="container ">
+  <div class="container">
     <!-- 面包屑 -->
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{ categoryData.parentName }}
+        <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }"
+          >{{ categoryData.parentName }}
         </el-breadcrumb-item>
         <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
-         <!-- 商品列表-->
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
+        <!-- 商品列表-->
+        <goods-item v-for="good in goodList" :good="good" :key="good.id" />
       </div>
     </div>
   </div>
-
 </template>
 
 
@@ -94,7 +127,5 @@ onMounted(() =>{
     display: flex;
     justify-content: center;
   }
-
-
 }
 </style>
