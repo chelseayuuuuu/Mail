8a81e70a153273471a7d17595e1ac76152aac841
2289580381@qq.com <2289580381@qq.com>
2023-10-02 11:22:29 +0800

    登录-401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { ElMessage } from 'element-plus';
 import 'element-plus/theme-chalk/el-message.css';
 import { useUserStore } from '@/stores/user';
+import router from '@/router';
 
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -27,10 +28,16 @@ httpInstance.interceptors.request.use(
 httpInstance.interceptors.response.use(
   (res) => res.data,
   (e) => {
+    const userStore = useUserStore();
     ElMessage({
       type: 'warning',
       message: e.response.data.message,
     });
+    //401Token失效处理 1、清除本地用户数据 2、跳转登录页
+    if (e.response.status === 401) {
+      userStore.clearUserInfo();
+      router.push('/login');
+    }
     return Promise.reject(e);
   }
 );
