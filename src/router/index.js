cbb705373ba02366676edeee9def44b845a3cbdf
2289580381@qq.com <2289580381@qq.com>
2023-10-02 18:42:56 +0800

    列表购物车-基础渲染

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
-import Login from '@/views/Login/index.vue'
-import Layout from '@/views/Layout/index.vue'
-import Home from '@/views/Home/index.vue'
-import Category from '@/views/Category/index.vue'
-import SubCategory from '@/views/SubCategory/index.vue'
-import Detail from '@/views/Detail/index.vue'
+import Login from '@/views/Login/index.vue';
+import Layout from '@/views/Layout/index.vue';
+import Home from '@/views/Home/index.vue';
+import Category from '@/views/Category/index.vue';
+import SubCategory from '@/views/SubCategory/index.vue';
+import Detail from '@/views/Detail/index.vue';
+import CartList from '@/views/CartList/index.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,32 +17,35 @@ const router = createRouter({
       children: [
         {
           path: '',
-          component: Home
+          component: Home,
         },
         {
           path: 'category/:id',
-          component: Category
+          component: Category,
         },
         {
           path: 'category/sub/:id',
-          component: SubCategory
+          component: SubCategory,
         },
         {
           path: 'detail/:id',
-          component: Detail
-        }
-      ]
+          component: Detail,
+        },
+        {
+          path: 'cartlist',
+          component: CartList,
+        },
+      ],
     },
     {
       path: '/login',
-      component: Login
-    }
-    
+      component: Login,
+    },
   ],
   // 定制路由滚动行为
-  scrollBehavior(){
-    return { top: 0 }
-  }
-})
+  scrollBehavior() {
+    return { top: 0 };
+  },
+});
 
-export default router
+export default router;
